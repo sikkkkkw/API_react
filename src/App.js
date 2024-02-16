676004c37dda3742d1a42b85d57c5e1cd2d52857
sikkkkkw <gksmsla92@naver.com>
2024-02-16 11:07:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+
+import Tawk from "./Tawk";
+
+// const URL = "http://infuser.odcloud.kr/oas/docs?namespace=15052602/v1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full">
+      <Tawk />
     </div>
   );
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  // const fetchData = async () => {
+  //   try {
+  //     setError(null);
+  //     setData(null);
+  //     setLoading(true);
+
+  //     const response = await axios.get(URL, {
+  //       params: {
+  //         serviceKey: process.env.REACT_APP_API_KEY,
+  //         numOfRows: 1,
+  //         pageNo: 10,
+  //       },
+  //     });
+  //     console.log(response.data);
+  //     setData(response.data);
+  //   } catch (e) {
+  //     setError(e);
+  //   }
+  //   setLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // if (loading) return <div>Loading...</div>;
+  // if (error) return <div>Error...</div>;
+  // if (!data) return null;
+  // return (
+  //   <div className=" w-full bg-red-500 flex justify-center">
+  //     {data.info.title}
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/Tawk.jsx
@@ -0,0 +1,10 @@
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
+export default function Tawk() {
+  return (
+    <TawkMessengerReact
+      propertyId="65cea87b8d261e1b5f610de6"
+      widgetId="1hmnk4900"
+    />
+  );
+}
